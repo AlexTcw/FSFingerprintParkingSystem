9a79dc8c801f533fb04cfa56bfbfddfd8b7eb09e
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-09-16 17:14:11 -0600

    corrijo zona horaria

--- ParkingFPAAccessManager/src/main/java/com/fingerprint/parkingfpaaccessmanager/ParkingFpaAccessManagerApplication.java
+++ ParkingFPAAccessManager/src/main/java/com/fingerprint/parkingfpaaccessmanager/ParkingFpaAccessManagerApplication.java
@@ -1,5 +1,4 @@
 package com.fingerprint.parkingfpaaccessmanager;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class ParkingFpaAccessManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParkingFpaAccessManagerApplication.class, args);
     }
-
 }
