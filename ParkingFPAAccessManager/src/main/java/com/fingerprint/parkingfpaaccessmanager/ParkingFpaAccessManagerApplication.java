8b7408a502b925f5625350d9c43344692e67ae95
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-09-16 17:31:51 -0600

    trato de resolver la zona horaria de nuevo

--- ParkingFPAAccessManager/src/main/java/com/fingerprint/parkingfpaaccessmanager/ParkingFpaAccessManagerApplication.java
+++ ParkingFPAAccessManager/src/main/java/com/fingerprint/parkingfpaaccessmanager/ParkingFpaAccessManagerApplication.java
@@ -1,6 +1,10 @@
 package com.fingerprint.parkingfpaaccessmanager;
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ParkingFpaAccessManagerApplication {
@@ -8,4 +12,11 @@ public class ParkingFpaAccessManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParkingFpaAccessManagerApplication.class, args);
     }
+
+    @PostConstruct
+    public void init(){
+        // Establecer la zona horaria predeterminada
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
+    }
+
 }
